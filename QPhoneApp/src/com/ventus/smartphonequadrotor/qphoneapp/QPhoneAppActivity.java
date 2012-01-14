570e19f772cbdb9985b42a144bcaa8a1d68766b1
Abhin Chhabra <chhabra.abhin@gmail.com>
2012-01-14 15:36:40 -0700

    Created the initial empty projects

---
+++ QPhoneApp/src/com/ventus/smartphonequadrotor/qphoneapp/QPhoneAppActivity.java
@@ -0,0 +1,13 @@
+package com.ventus.smartphonequadrotor.qphoneapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QPhoneAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
